10efec20b48a74fd932b0e501dbe61eb5d5889a9
Carson Wright <carsonhwright@gmail.com>
2026-03-28 15:24:30 -0400

    I guess copilot isn't so bad, definitely helps when debugging is this bad

--- simple.c
+++ simple.c
@@ -9,21 +9,27 @@ static LIST_HEAD(birthday_list);
 /* This function is called when the module is loaded
 */
 static struct birthday* make_person_proto(void) {
-	struct birthday *person;
-	person = kmalloc(sizeof(*person), GFP_KERNEL);
-	person->day = 2;
-	person->month = 8;
-	person->year = 1995;
-	INIT_LIST_HEAD(&person->list);
-	list_add_tail(&person->list, &birthday_list);
-	return person;
+	for (int i = 0; i < 5; i++) {
+		struct birthday* person;
+		person = kmalloc(sizeof(*person), GFP_KERNEL);
+		person->day = i + 1;
+		person->month = i + 2;
+		person->year = 1995 + i;
+		INIT_LIST_HEAD(&person->list);
+		list_add_tail(&person->list, &birthday_list);
+	}
+	return list_first_entry(&birthday_list, struct birthday, list);
 }
 
 void traverse_list(struct birthday* person) {
-	struct birthday* ptr = person;
-	list_for_each_entry(ptr, &birthday_list, ptr->list) {
-		struct birthday *obj = list_entry(ptr, struct birthday, ptr->list);
-		printk(KERN_INFO "what am I looking at %d\n", obj);
+	struct birthday *ptr, *n;
+	list_for_each_entry_safe(ptr, n, &birthday_list, list) {
+		// struct birthday *obj = list_entry(ptr, struct birthday, ptr->list);
+		printk(KERN_INFO "Day: %d\n", ptr->day);
+		printk(KERN_INFO "Month: %d\n", ptr->month);
+		printk(KERN_INFO "Year: %d\n", ptr->year);
+		list_del(&ptr->list);
+		kfree(ptr);
 	}
 	return;
 }
@@ -33,8 +39,6 @@ static int __init simple_init(void)
 	printk(KERN_INFO "Loading Module\n");
 	struct birthday *proto_person = make_person_proto();
 	traverse_list(proto_person);
-	// the newline char at the end will block output for dmesg, be careful
-	printk(KERN_INFO "day: %d\n", proto_person->day);
 	return 0;
 }
 
